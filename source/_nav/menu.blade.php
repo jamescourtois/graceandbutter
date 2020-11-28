c14f0e3821fd767617339c039558189f50b3c358
Jim Courtois <jim@Jims-MacBook-Pro.local>
2020-11-27 20:34:54 -0600

    Initial commit w/ jigsaw

---
+++ source/_nav/menu.blade.php
@@ -0,0 +1,31 @@
+<nav class="hidden lg:flex items-center justify-end text-lg">
+    <a title="{{ $page->siteName }} Blog" href="/food"
+        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/blog') ? 'active text-blue-600' : '' }}">
+        Food
+    </a>
+
+    <a title="{{ $page->siteName }} Blog" href="/events"
+        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/blog') ? 'active text-blue-600' : '' }}">
+        Events
+    </a>
+
+    <a title="{{ $page->siteName }} Blog" href="/experiences"
+        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/blog') ? 'active text-blue-600' : '' }}">
+        Experiences
+    </a>
+    
+    <a title="{{ $page->siteName }} Blog" href="/blog"
+        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/blog') ? 'active text-blue-600' : '' }}">
+        Blog
+    </a>
+
+    <a title="{{ $page->siteName }} About" href="/about"
+        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/about') ? 'active text-blue-600' : '' }}">
+        About
+    </a>
+
+    <a title="{{ $page->siteName }} Contact" href="/contact"
+        class="ml-6 text-gray-700 hover:text-blue-600 {{ $page->isActive('/contact') ? 'active text-blue-600' : '' }}">
+        Contact
+    </a>
+</nav>
---
+++ source/about.blade.php
@@ -0,0 +1,21 @@
+---
+title: About
+description: A little bit about the site
+---
+@extends('_layouts.master')
+
+@section('body')
+    <h1>About</h1>
+
+    <img src="/assets/img/about.png"
+        alt="About image"
+        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+
+        <p class="mb-6">Everyone had a birthday in the year of 2020. Some got creative and celebrated in safe ways, some skipped the hoopla all together, some were forced to be alone, some had to cancel plans and trips that have been in place for years. But everyone had a birthday. A reason to celebrate. A day you are here, when so many are not. All moments we are present, with the ones we love, deserve to be celebrated. Birthdays, Anniversaries, and Tuesday nights.</p>
+
+        <p class="mb-6">I love making food for people. It surpasses love language. It feels like destiny, like ultimate fulfillment. Even when I sit down to eat lunch by myself, I set the table and plate my food. I've been baking a few years and eating baked goods a few more years than that. I looooove sweets. If it comes down to chocolate or potato chips, you better cover that chip in chocolate.</p>
+
+        <p class="mb-6">I want to help you plan your engagement. I want to pack a picnic for you. I want to provide a dessert bar for your wedding. I want to make your family a lasagna. I want to teach you how to make scones. The possibilities are endless and we can navigate Covid-19 restrictions together.</p>
+
+        <p class="mb-6">Shoot me a message here(link to contact form). Let's chat!</p>
+@endsection
