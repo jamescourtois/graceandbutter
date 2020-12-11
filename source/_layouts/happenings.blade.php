43ea858707be67327ac30f1754d7aa8e33f990e2
Jim Courtois <jim@Jims-MacBook-Pro.local>
2020-12-11 01:04:12 -0600

    Can't stop won't stop

---
+++ source/_layouts/happenings.blade.php
@@ -0,0 +1,43 @@
+<div class="w-full flex flex-wrap justify-center mx-auto align-stretch border-white border-2">
+        
+<div class="w-full lg:w-1/2 text-center items-stretch justify-center flex">
+        <div class="relative w-full p-8 text-white border-4 border-white flex items-center justify-center py-16 lg:py-24">
+            
+            <div class="relative z-20 p-8">
+                
+                <div class="relative flex flex-col items-center justify-center z-20">
+                    <h3>Grace and Butter <br> Pie Pop-up!</h3>
+                    <span class="block mb-4">December 12 and December 19 <br>@ Sweet Things Sweet Shop in St. Genevieve!</span>
+                    <a class="btn" target="_blank" href="https://stegencandy.square.site">Details and Ordering Here</a>
+                </div>
+                <div class="bg-img bg-red opacity-85 z-10"></div>
+            </div>
+            <div class="bg-img  bottom">
+                <img  src="/assets/img/pie.jpg" alt="Christmas Cookies">
+            </div>
+        </div>
+    </div> 
+<div class="w-full lg:w-1/2 text-center items-stretch justify-center flex">
+        <div class="relative w-full p-8 text-white border-4 border-white flex items-center justify-center py-16 lg:py-24">
+            
+            <div class="relative z-20 p-8">
+                
+                <div class="relative flex flex-col items-center justify-center z-20">
+                    <h3>Christmas Cookie Box!</h3>
+                    <ul class="text-left pl-4">
+                        <li>Three (3) Chocolate Krinkle</li>
+                        <li>Three (3) Gingerbread People</li>
+                        <li>Three (3) Oma's Butter Cookies</li>
+                        <li>Three (3) Raspberry Almond Thumbprints</li>
+                    </ul>
+                    <a class="btn" href="/cookie-order">Order Cookie Box</a>
+                </div>
+                <div class="bg-img bg-red opacity-85 z-10"></div>
+            </div>
+            <div class="bg-img  bottom">
+                <img  src="/assets/img/cookiebox.jpg" alt="Christmas Cookies">
+            </div>
+        </div>
+    </div> 
+    
+</div>
--- source/index.blade.php
+++ source/index.blade.php
@@ -9,7 +9,7 @@
         <div class="container mx-auto flex flex-wrap">
             <div class="w-full">
                 <img src="/assets/img/mandi.jpg" alt="Portrait of Mandi Lee of Grace and Butter" class="mr-4 md:mr-16 mb-2 md:mb-8 max-w-40 md:max-w-33 lg:max-w-250 float-left">
-                <h2>Hi! I'm Mandi.</h2>
+                <h2 class="md:text-5xl">Hi! I'm Mandi.</h2>
                 <p>Everyone had a birthday in the year of 2020. Some got creative and celebrated in safe ways, some skipped the hoopla all together, some were forced to be alone, some had to cancel plans and trips that have been in place for years. But everyone had a birthday. A reason to celebrate. A day you are here, when so many are not. All moments we are present, with the ones we love, deserve to be celebrated. Birthdays, Anniversaries, and Tuesday nights.</p>
         
                 <p>I love making food for people. It surpasses love language. It feels like destiny, like ultimate fulfillment. Even when I sit down to eat lunch by myself, I set the table and plate my food. I've been baking a few years and eating baked goods a few more years than that. I looooove sweets. If it comes down to chocolate or potato chips, you better cover that chip in chocolate.</p>
