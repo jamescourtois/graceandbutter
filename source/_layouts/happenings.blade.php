294fc514a9e2a0f1781aa7cbeae685aa6edec460
Jim Courtois <jcourtois@atomicdust.com>
2021-04-25 12:14:58 -0500

    Adds schedule

--- source/_layouts/happenings.blade.php
+++ source/_layouts/happenings.blade.php
@@ -1,5 +1,45 @@
 <div class="row">
     <div class="content">
+    <h2>Your Next Chance to Visit Grace & Butter!</h2>
+    <p class="h2">May 22, 2021 </p>
+    <p class="h4">Cape Riverfront Farmer’s Market</p>
+    <p class="h4">8 a.m. - 12 p.m.</p>
+    <a class="text-white hover:text-light-teal" href="https://www.google.com/maps/place/35+S+Spanish+St,+Cape+Girardeau,+MO+63703/data=!4m2!3m1!1s0x88778785c1025afb:0xbf21bb070915394b?sa=X&ved=2ahUKEwjT5dfu75nwAhWLW80KHUl4DDIQ8gEwAHoECAYQAQ">
+        35 S Spanish St, Cape Girardeau, MO 63703
+    </a>
+    </div>
+    <div class="image">
+        <div class="bg-img">
+            <img src="/assets/img/ccbread.jpg" alt="Apple Pie Photo">
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="content">
+        <h2>More Upcoming G&B Events</h2>
+        <p>More Details will be posted soon!</p>
+        <ul>
+            <li>June 5 2021 - Supper Experience STL Ivory Orange</li>
+            <li>June 6 2021 - Tea Party STL Ivory Orange</li>
+            <li>June 19 2021 - Cape Riverfront Farmer’s Market</li>
+            <li>June 26 + 27 2021 - Honey Festival Ste Gen</li>
+            <li>July 10 2021 - Rock n Roll Drive In Farmer’s Market</li>
+            <li>July 31 2021 - Cape Riverfront Farmer’s Market</li>
+            <li>August 14 2021 - Rock n Roll Drive In Farmer’s Market</li>
+            <li>August 21 2021 - Annie Em’s</li>
+            <li>August 28 2021 - Cape Riverfront Farmer’s Market</li>
+            <li>September 18 2021 - Cape Riverfront Farmer’s Market</li>
+            <li>October 9 2021 - Cape Riverfront Farmer’s Market</li>
+        </ul>
+    </div>
+    <div class="image">
+        <div class="bg-img">
+            <img src="/assets/img/bbmuffin.jpg" alt="Apple Pie Photo">
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="content">
         <h2>Classics Menu</h2>
         <ul class="text-left pl-4">
             <li>Cookies by the Dozen</li>
