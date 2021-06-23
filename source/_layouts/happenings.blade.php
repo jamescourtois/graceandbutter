3c1a0157eb6c200fd3e17f0ed66563b3cc6c1ac0
Jim Courtois <thatonejimguy@gmail.com>
2021-06-22 21:00:21 -0500

    Updooots

--- source/_layouts/happenings.blade.php
+++ source/_layouts/happenings.blade.php
@@ -1,45 +1,8 @@
 <div class="row">
     <div class="content">
-    <h2>Supper with Grace & Butter!</h2>
-    <p class="h2">Saturday June 5, 2021 @ 6 p.m.</p>
-    <a 
-    href="https://www.google.com/maps/place/Ivory+Orange/@38.5488439,-90.2622034,15z/data=!4m2!3m1!1s0x0:0x199f22acf1f085d5?sa=X&ved=2ahUKEwjAmJCh1rLwAhUKOs0KHYTKD1IQ_BIwG3oECEcQBQ" 
-    class="h4" target="_blank">
-        Ivory Orange
-    </a>
-    <p>(Registration is Closed)</p>
-    </div>
-    <div class="image">
-        <div class="bg-img">
-            <img src="/assets/img/dinner.jpg" alt="Dinner Photo">
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="content">
-    <h2>Tea Party with Grace & Butter!</h2>
-    <p class="h2">Sunday June 6, 2021 @ 2 p.m.</p>
-    
-    <a 
-    href="https://www.google.com/maps/place/Ivory+Orange/@38.5488439,-90.2622034,15z/data=!4m2!3m1!1s0x0:0x199f22acf1f085d5?sa=X&ved=2ahUKEwjAmJCh1rLwAhUKOs0KHYTKD1IQ_BIwG3oECEcQBQ" 
-    class="h4" target="_blank">
-        Ivory Orange
-    </a>
-    <p>(Registration is Closed)</p>
-    </div>
-    <div class="image">
-        <div class="bg-img">
-            <img src="/assets/img/teapot.jpg" alt="Tea Pot Photo">
-        </div>
-    </div>
-</div>
-<div class="row">
-    <div class="content">
-        <h2>More Upcoming G&B Events</h2>
+        <h2>Upcoming G&B Events</h2>
         <p>More Details will be posted soon!</p>
-        <ul>
-            <li>June 19 2021 - Cape Riverfront Farmer’s Market</li>
-            <li>June 26 + 27 2021 - Honey Festival Ste Gen</li>
+        <ul>          
             <li>July 10 2021 - Rock n Roll Drive In Farmer’s Market</li>
             <li>July 31 2021 - Cape Riverfront Farmer’s Market</li>
             <li>August 14 2021 - Rock n Roll Drive In Farmer’s Market</li>
