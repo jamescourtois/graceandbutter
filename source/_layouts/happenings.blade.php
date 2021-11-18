be7ce4c1e30d1daf19dcf6273f7d63bdc8639779
James Courtois <jamescourtois@Jamess-MacBook-Pro.local>
2021-11-18 11:51:59 -0600

    Latest shit

--- source/_layouts/happenings.blade.php
+++ source/_layouts/happenings.blade.php
@@ -1,32 +1,50 @@
-<div class="row bg-dark-teal">
-    <div class="content">
-        <h2>Upcoming G&B Events</h2>
-        <ul>          
-            <li>December 12 2021 - Holiday Smarket at Ivory Orange</li>
-        </ul>
-    </div>
-    <div class="image min-h-500 lg:min-h-75vh">
-        <div class="bg-img">
-            <img src="/assets/img/weddingcake.jpg" alt="Wedding Cake">
+<div class="container max-w-6xl mx-auto">
+    <div class="row pt-8">
+        <div class="content">
+            <h2>Christmas Goodies!</h2>
+            <ul>
+                <li>Cookie Box $25</li>
+                <li>Charcuterie Platter $50</li>
+                <li>Cheesecake Plain $25, Topped $35</li>
+            </ul>
+            <a class="btn order" href="#contact">Submit Order Request</a>
+        </div>
+        <div class="image">
+            <div class="bg-img contain">
+                <img src="/assets/img/xmas2021.png" alt="Christmas Cookies">
+            </div>
         </div>
     </div>
-</div>
-<div class="row">
-    <div class="content">
-        <h2>Classics Menu</h2>
-        <ul class="text-left pl-4">
-            <li>Cookies by the Dozen</li>
-            <li>Pie - Apple or Cherry</li>
-            <li>Lasagna (take and bake)</li>
-            <li>Cheesecake</li>
-            <li>Gluten-free Brownies</li>
-        </ul>
-        <a class="btn order" href="#contact">Submit Order Request Form</a>
+    <div class="row">
+        <div class="content">
+            <h2>Upcoming G&B Events</h2>
+            <ul>
+                <li>December 12 2021 - Holiday Smarket at Ivory Orange</li>
+            </ul>
+        </div>
+        <div class="image">
+            <div class="bg-img contain">
+                <img src="/assets/img/weddingcake.jpg" alt="Wedding Cake">
+            </div>
+        </div>
     </div>
+    <div class="row">
+        <div class="content">
+            <h2>Classics Menu</h2>
+            <ul class="text-left pl-4">
+                <li>Cookies by the Dozen</li>
+                <li>Pie - Apple or Cherry</li>
+                <li>Lasagna (take and bake)</li>
+                <li>Cheesecake</li>
+                <li>Gluten-free Brownies</li>
+            </ul>
+            <a class="btn order" href="#contact">Submit Order Request</a>
+        </div>
     
-    <div class="image">
-        <div class="bg-img">
-            <img src="/assets/img/applepie.jpg" alt="Apple Pie Photo">
+        <div class="image">
+            <div class="bg-img contain">
+                <img src="/assets/img/applepie.jpg" alt="Apple Pie Photo">
+            </div>
         </div>
     </div>
 </div>
