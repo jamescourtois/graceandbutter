53218816e5c10ba5499a1e28b6294d2ff212dc3d
James Courtois <jamescourtois@Jamess-MacBook-Pro.local>
2022-01-27 20:28:48 -0600

    Adds google anal yitcs:

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-KBG5ZT6CSN"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-KBG5ZT6CSN');
+        </script>
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
