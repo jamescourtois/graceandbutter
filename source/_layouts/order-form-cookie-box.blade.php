c41483ab168c2181fabff102a1b32a68e4d2725c
James Courtois <jamescourtois@Jamess-MacBook-Pro.local>
2022-05-17 22:02:18 -0500

    Latest

---
+++ source/_layouts/order-form-cookie-box.blade.php
@@ -0,0 +1,143 @@
+
+
+
+<div class="formbox bg-dark-teal py-16 lg:py-24">
+  <div class="container flex flex-wrap mx-auto px-4">
+      
+      <div class="w-full lg:w-1/3">
+          <h1 class="text-white mb-8 text-4xl md:text-5xl">Get Your Cookie Box of the Month!</h1>
+          <p class="text-white">Fill out this form and I will be in touch to sort out the payment and delivery details. Thank you!</p>
+          <ul class="text-left pl-4 text-white">
+            <li>Cookies by the dozen shipped on the 15th of each month</li>
+            <li>$20 plus shipping</li>
+            <li>Free pick up in the STL area</li>
+            <li>No commitments, just fun flavors!</li>
+            <li>Order deadline is the 10th of each month</li>
+        </ul>
+        <div class="mb-16">
+            <img src="/assets/img/mandi-with-cookies.jpg" alt="Apple Pie Photo">
+        </div>
+      </div>
+      
+      <div class="w-full lg:w-2/3 lg:pl-24">
+          <h3 class="text-white">Cookie Box Sign Up Form</h3>
+          <form id="contact" action="https://api.form-data.com/f/t35iq57k62hnz3qake1rj" method="post" >
+              
+          
+              <div class="flex flex-wrap mb-6 -mx-3">
+                  <div class="w-full lg:w-1/2 mb-6 lg:mb-0 px-3">
+                      <label class="block mb-2 text-sm font-semibold text-white" for="name">
+                          Name*
+                      </label>
+          
+                      <input
+                          type="text"
+                          id="name"
+                          placeholder="Enter your name"
+                          name="name"
+                          class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
+                          required
+                      >
+                  </div>
+          
+                  <div class="w-full px-3 lg:w-1/2">
+                      <label class="block text-sm font-semibold mb-2 text-white" for="email">
+                          Email Address*
+                      </label>
+          
+                      <input
+                          type="email"
+                          id="email"
+                          placeholder="Enter your email"
+                          name="email"
+                          class="block w-full border shadow rounded-lg outline-none mb-2 px-4 py-3"
+                          required
+                      >
+                  </div>
+              </div>
+
+              <div class="w-full float-left mb-6">
+                <div class="text-sm font-semibold mb-2 text-white">Pick it up, or ship it?*</div>
+                <input
+                type="radio"
+                id="delivery"
+                required
+                name="pickup_or_ship"
+                value="ship">
+                <label class="text-sm font-semibold mb-2 text-white" for="delivery">
+                    Ship my order (Shipping fee applies)
+                </label>
+                <br />
+                <input
+                type="radio"
+                id="pickup"
+                required
+                name="pickup_or_ship"
+                value="pickup">
+                <label class="text-sm font-semibold mb-2 text-white" for="pickup">
+                    I'll pick it up
+                </label>
+            </div>
+
+            <div class="w-full float-left mb-6">
+                <div class="text-sm font-semibold mb-2 text-white">How many dozens?*</div>
+                <input
+                type="radio"
+                id="how_many_dozens"
+                required
+                name="how_many_dozens"
+                value="1 dozen ">
+                <label class="text-sm font-semibold mb-2 text-white" for="how_many_dozens">
+                    1 dozen 
+                </label>
+                <br />
+                <input
+                type="radio"
+                id="pickup"
+                required
+                name="how_many_dozens"
+                value="2 dozen ">
+                <label class="text-sm font-semibold mb-2 text-white" for="pickup">
+                    2 dozen 
+                </label>
+                <br />
+                <input
+                type="radio"
+                id="pickup"
+                required
+                name="how_many_dozens"
+                value="3 dozen ">
+                <label class="text-sm font-semibold mb-2 text-white" for="pickup">
+                    3 dozen 
+                </label>
+            </div>
+          
+              <div class="w-full mb-12">
+                  <label class="block text-sm font-semibold mb-2 text-white" for="message">
+                      Any Comments/Questions? (Optional)
+                  </label>
+          
+                  <textarea
+                      id="comment"
+                      rows="4"
+                      name="comment"
+                      class="block w-full border shadow rounded-lg outline-none appearance-none mb-2 px-4 py-3"
+                      placeholder="Write your comment or question here."
+                  ></textarea>
+              </div>
+
+              <input type="text" name="xx_password" class="sr-only" tabindex="-1" autocomplete="off">
+          
+          
+              <div class="flex justify-start w-full">
+                  <input
+                      type="submit"
+                      value="Subscribe"
+                      class="btn"
+                  >
+              </div>
+          </form>
+      </div>
+  </div>
+</div>
+
---
+++ source/order-cookie-box.blade.php
@@ -0,0 +1,12 @@
+---
+title: Cookie Subscription Box Order From
+description: Order your Cookie Subscription Box! Thanks for supporting Grace & Butter!
+---
+
+@extends('_layouts.master')
+
+@section('body')
+    
+   @include('_layouts.order-form-cookie-box')
+    
+@stop
