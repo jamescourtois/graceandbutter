d37e1ad2bccac9944ce523d4a2b66b2d43174130
jamescourtois <james.r.courtois@gmail.com>
2023-12-04 12:14:23 -0600

    xmas stuff

--- source/_layouts/happenings.blade.php
+++ source/_layouts/happenings.blade.php
@@ -1,20 +1,44 @@
-
-
+<div class="bg-dark-teal text-white">
+	
+	
+	<div class="row max-w-5xl pt-8 lg:py-8 mx-auto">
+		<div class="content dark">
+			<h2 class="text-light-teal">Christmas Menu 2023</h2>
+			<p>Mark your calendars for December 23. It will be GB’s last pop-up of the year! Peep that menu. This will be a sell out situation so PRE-ORDER!</p>
+	
+			<p>And yes, all these items are shippable!</p>
+			<ul>
+				<li>Gingerbread Loaf</li>
+				<li>Christmas Krispies</li>
+				<li>Chocolate Crinkles</li>
+				<li>Candied Pecans</li>
+			</ul>
+			<a class="btn order" href="#form">Submit an Order</a>
+		</div>
+		<div class="image">
+			<img src="/assets/img/xmasmenu23.jpg"  alt="Christmas Menu 2023">
+		</div>
+	</div>
+</div>
 <div class="row max-w-5xl pt-8 lg:py-8 mx-auto">
-    <div class="content text-center">
-        <h2>Pumpkin Bread!</h2>
-        <ul class="text-left pl-4">
-            <li>$15 per delicious loaf</li>
-            <li>The 9"x13" size is perfect for sharing!</li>
-            <li>Topped with tasty pepitas (optional)</li>
-            <li>Pick up at Annie Em's at Home in Cape Girardeau</li>
+    <div class="content text-left">
+        <h2 class="text-center">Missouri Small Business Christmas Bonus!</h2>
+        <p>I wanted to do something fun for Missouri small businesses this year. I’m giving away a Christmas Bonus of $100!
+				<br /><br />To Nominate A Business:</p>
+        <ul>
+					<li>Follow @graceandbutter</li>
+					<li>Like <a href="https://www.instagram.com/p/C0T9wH5unzT/">my post for the Christmas Bonus</a> </li>
+					<li>Tag a Missouri Small Business in the comments on Instagram that you feel is deserving of a Christmas Bonus.</li>
         </ul>
-        <a class="btn order" href="#form">Order</a>
+        <p>That’s it! I’ll randomly select a winner on Christmas Day.</p>
+        </ul>
+        <!-- @todo add button action -->
+        <a class="btn order" href="https://www.instagram.com/p/C0T9wH5unzT/">Go to Instagram</a>
     </div>
     <div class="image">
-        
-            <a href="#form"><img src="/assets/img/pumpkin-bread.png" alt="pumpkin bread"></a>
-        
+    	<a href="https://www.instagram.com/p/C0T9wH5unzT/">
+    		<img src="/assets/img/xmasbonus.png" alt="Christmas bonus graphic">
+    	</a>
     </div>
 </div>
 
