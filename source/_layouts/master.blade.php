0fad55a054080ba8b9600d9ea0f509eedb5a10e0
jamescourtois <james.r.courtois@gmail.com>
2024-03-24 13:58:44 -0500

    The end

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -32,43 +32,29 @@
     </head>
 
     <body class="flex flex-col justify-between min-h-screen">
-        <header class="flex justify-center items-center py-2 bg-white shadow-lg z-50 relative" role="banner">
+        <header role="banner">
            
                 
-                    <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center justify-center hover:no-underline">
-                        
-                        <div class="site-logo">
-                            @include('_nav.site-logo-svg')
-                        </div>
-                        <span class="sr-only">{{ $page->siteName }}</span>
-                    </a>
-                    <a class="no-underline hover:no-underline" href="/"><h1 class="text-red inline-flex items-center justify-start text-3xl leading-tight lg:text-5xl m-0 p-0 pl-4">Food Experiences <br class="lg:hidden"/>by Mandi Lee</h1></a>
-                    
            
         </header>
 
         
 
-        <main role="main" class="flex-auto relative z-10">
-            @yield('body')
+        <main role="main" class="flex-auto relative z-10 flex items-center justify-center">
+				<div>
+					<div class="flex items-center justify-center pb-5">
+						@include('_nav.site-logo-svg')
+					</div>
+					<div class="w-full text-center px-5" style="max-width:450px;">
+						<h1 class="text-red">Goodbye &amp; Thank You for Everything!</h1>
+						<p>It's time for Grace &amp; Butter to come to an end. Don't be sad to see the end - be happy for the special moments and priceless memories. Carry on the spirit of G&amp;B by supporting your local businesses and sharing simple pleasures with the ones you love.</p>
+					</div>
+				</div>
         </main>
-        @if ($page->getPath() == '')
-            @include('_layouts.form')
-        @endif
+        
 
         <footer class="text-center text-sm py-4" role="contentinfo">
-            <ul class="flex flex-col justify-center list-none m-0">
-                <li class="md:mr-2">
-                    &copy; {{ date('Y') }} <a href="http://graceandbutter.com" title="Mandi Lee's website">Grace & Butter by Mandi Lee</a>.
-                </li>
-
-                <li>
-                    Built by <a target="_blank" title="I developed this website" href="https://jamescourtois.surge.sh">James Courtois</a> with <a href="http://jigsaw.tighten.co" target="_blank" title="Jigsaw by Tighten">Jigsaw</a>
-                    and <a target="_blank" href="https://tailwindcss.com" title="Tailwind CSS, a utility-first CSS framework">Tailwind CSS</a>. 
-                    Hosted and deployed using <a target="_blank" title="This is where my website lives" href="https://surge.sh">Surge</a>.
-                    Forms powered by <a href="https://form-data.com" target="blank">form-data.com</a>. Form data is deleted after 90 days.
-                </li>
-            </ul>
+            
         </footer>
 
         <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
